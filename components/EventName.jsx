ed91d23aa5401a4763a984e8dde13581492075a0
addison-codes <hi@addison.codes>
2022-09-10 00:24:32 -0400

    chore: :tada: Initialize project and create component structure

---
+++ components/EventName.jsx
@@ -0,0 +1,11 @@
+
+const EventName = () => {
+  return (
+    <div className="mx-auto">
+      <label htmlFor="eventName" className="m-4" placeholder="Enter Event Name">Event Name</label>
+      <input label='Event Name' name='eventName'></input>
+    </div>
+  )
+}
+
+export default EventName
