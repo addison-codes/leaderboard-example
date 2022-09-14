76abb09d085a8ee91911185cfeb5a3f4f7e5222b
addison-codes <hi@addison.codes>
2022-09-14 15:51:50 -0400

    fix: update small components

--- components/EventName.jsx
+++ components/EventName.jsx
@@ -1,6 +1,6 @@
 const EventName = ({ name, onNameChange }) => {
   return (
-    <div className="text-center mt-20">
+    <div className="mt-20 text-center">
       <label htmlFor="eventName" className="mr-4 text-xl">
         Event Name:
       </label>
--- components/RangeSelect.jsx
+++ components/RangeSelect.jsx
@@ -1,10 +1,10 @@
-const RangeSelect = () => {
+const RangeSelect = ({ range, updateRange }) => {
   return (
-    <div className="text-center mt-20">
+    <div className="mt-20 text-center">
       <label className="mr-4 text-xl" htmlFor="range">
         Leaderboard Range:
       </label>
-      <select name="range" className="p-2 rounded">
+      <select name="range" className="p-2 rounded" onChange={updateRange}>
         <option value="100">Top 100</option>
         <option value="global">Global Leaderboard</option>
       </select>
